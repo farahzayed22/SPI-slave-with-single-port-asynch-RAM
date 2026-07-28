0919e3093b9bc365a044988e6581b1afe7f767f1
farahzayed22 <farahzayed418@gmail.com>
2026-07-27 23:57:17 -0700

    first spi

---
+++ SPI_slave.v
@@ -0,0 +1,157 @@
+module SPI_slave(mosi, ss_n ,rx_data, rx_valid, clk, rstn, tx_data, tx_valid, miso);
+    input mosi, ss_n, clk, rstn;
+    input [9:0]tx_data;
+    input tx_valid;
+    output reg [7:0] rx_data;
+    output reg rx_valid, miso;
+
+    localparam IDLE=3'b000;
+    localparam WRITE=3'b001;
+    localparam READ_DATA=3'b010;
+    localparam READ_ADD=3'b011;
+    localparam CHK_CMD=3'b100;
+
+    reg [2:0] current_state, next_state;
+    reg [3:0] counter;
+    reg [9:0] serial2Parallel;
+    reg [7:0] parallel2Serial;
+    reg addr_received;
+
+    always @(posedge clk) begin
+        if(~rstn)
+            current_state<=IDLE;
+        else
+            current_state<=next_state;
+    end
+
+    always @(*) begin 
+        case(current_state)
+        IDLE:begin
+            if(ss_n) next_state<=IDLE;
+            else next_state<=CHK_CMD;
+        end
+        WRITE:begin
+            if(ss_n) next_state<=IDLE;
+            else next_state<=WRITE;
+        end
+        READ_DATA:begin
+            if(ss_n) next_state<=IDLE;
+            else next_state<=READ_DATA;
+        end
+        READ_ADD:begin
+            if(ss_n) next_state<=IDLE;
+            else next_state<=READ_ADD;
+        end
+        CHK_CMD:begin
+            if(ss_n) next_state<=IDLE;
+            else begin
+                if(~mosi) next_state<=WRITE;
+                else begin
+                    if(addr_received) next_state<=READ_DATA;
+                    else next_state<=READ_ADD;
+                end
+            end 
+        end
+        default: next_state<=IDLE;
+        endcase
+    end
+
+
+    always @(posedge clk) begin
+        if(~rstn)begin
+            counter<=0;
+            serial2Parallel<=0;
+            parallel2Serial<=0;
+            addr_received<=0;
+            rx_data<=0;
+            rx_valid<=0;
+            miso<=1'b0;
+        end 
+        else begin
+            case(current_state)
+            IDLE:begin
+                counter<=0;
+                serial2Parallel<=0;
+                parallel2Serial<=0;
+                addr_received<=0;
+                rx_data<=0;
+                rx_valid<=0;
+            end
+
+            WRITE:begin
+                if(counter<10)begin
+                    serial2Parallel<={serial2Parallel[8:0],mosi};
+                    counter<=counter+1;
+                    rx_valid<=0;
+                end
+                else begin
+                    rx_data<=serial2Parallel[7:0];
+                    rx_valid<=1;
+                    counter<=0;
+                end
+            end
+
+            READ_DATA:begin
+                if(tx_valid)begin
+                    rx_valid<=0;
+                    parallel2Serial<=tx_data;
+                    if(counter<8)begin
+                        miso<=parallel2Serial[7];
+                        parallel2Serial<={parallel2Serial[6:0],1'b0};
+                        counter<=counter+1;
+                    end
+                    else begin
+                        addr_received<=0;
+                        counter<=0;
+                    end  
+                end 
+                else begin
+                    if(counter<10)begin
+                        serial2Parallel<={serial2Parallel[8:0],mosi};
+                        counter<=counter+1;
+                        miso<=1'b0;
+                        rx_valid<=0;
+                    end
+                    else begin
+                        rx_data<=serial2Parallel[7:0];
+                        rx_valid<=1;
+                        counter<=0;
+                    end
+                end 
+            end
+
+            READ_ADD:begin
+                if(counter<10)begin
+                    serial2Parallel<={serial2Parallel[8:0],mosi};
+                    counter<=counter+1;
+                    rx_valid<=0;
+                end
+                else begin
+                    rx_data<=serial2Parallel[7:0];
+                    rx_valid<=1;
+                    addr_received<=1;
+                    counter<=0;
+                end
+            end
+            CHK_CMD:begin
+                counter<=0;
+                serial2Parallel<=0;
+                parallel2Serial<=0;
+                addr_received<=0;
+                rx_data<=0;
+                rx_valid<=0;
+                miso<=1'b0;
+            end
+            default:begin
+                counter<=0;
+                serial2Parallel<=0;
+                parallel2Serial<=0;
+                addr_received<=0;
+                rx_data<=0;
+                rx_valid<=0;
+                miso<=1'b0;
+            end
+            endcase
+        end 
+    end 
+endmodule
--- asynch_ram.v
+++ asynch_ram.v
@@ -3,13 +3,13 @@ module asynch_ram(din,clk,rstn,rx_valid,dout,tx_valid);
     parameter ADDR_SIZE=8;
     input [9:0] din;
     input clk,rstn,rx_valid;
-    output [7:0] dout;
-    output tx_valid;
+    output reg [7:0] dout;
+    output reg tx_valid;
     
     reg [ADDR_SIZE-1:0]wr_addr,rd_addr;
     reg [ADDR_SIZE-1:0] mem [0:MEM_DEPTH-1];
 
-    assign tx_valid= din[9]? 1: 0;
+    assign tx_valid= din[9] && din[8]? 1: 0;
     
     always @(posedge clk) begin 
         if(~rstn)begin
@@ -22,8 +22,8 @@ module asynch_ram(din,clk,rstn,rx_valid,dout,tx_valid);
             tx_valid<=0;
             if(rx_valid)begin
                 case(din[9:8])
-                2'b00: mem[wr_addr]<=din[7:0];
-                2'b01: din[7:0]<=wr_addr;
+                2'b00: wr_addr<=din[7:0];
+                2'b01: mem[wr_addr]<=din[7:0];
                 2'b10: rd_addr<=din[7:0];
                 2'b11: begin 
                     dout<=mem[rd_addr];
